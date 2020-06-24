1ec3afc295d9a4ad114970218b1c3a1d8f3b75da
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-06-23 21:53:59 -0400

    added indexdb

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,11 +2,12 @@ const FILES_TO_CACHE = [
 '/',
 '/index.html',
 '/index.js',
-'style.css',
-'icons/icon-192x192.png',
-'icond/icon-512x512.png'
+'/styles.css',
+'/icons/icon-192x192.png',
+'/icons/icon-512x512.png',
+'/manifest.webmanifest',
+'/db.js'
 ];
-
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
@@ -21,24 +22,6 @@ self.addEventListener("install", function(evt) {
     );
   
     self.skipWaiting();
-  });
-  
-  // activate
-  self.addEventListener("activate", function(evt) {
-    evt.waitUntil(
-      caches.keys().then(keyList => {
-        return Promise.all(
-          keyList.map(key => {
-            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-              console.log("Removing old cache data", key);
-              return caches.delete(key);
-            }
-          })
-        );
-      })
-    );
-  
-    self.clients.claim();
   });
   
   // fetch
